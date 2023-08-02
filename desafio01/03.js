2f697840db2df7c0bad7235259c284e7cea6239a
Natanael Oliveira <natanolveira151@gmail.com>
2023-08-02 13:59:17 -0300

    desafio 03

---
+++ desafio01/03.js
@@ -0,0 +1,9 @@
+var dinheiro = 80
+var  valor_tenis = 129.99
+var  valor01 = 100
+
+
+var porcentagem_valor = (dinheiro * valor01 ) /valor_tenis;
+
+console.log(" O Valor da porcentagem é ", porcentagem_valor)
+
