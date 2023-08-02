cc9a0e78291f2728a1e017c079468851028c48e5
Natanael Oliveira <natanolveira151@gmail.com>
2023-08-02 14:24:14 -0300

    desfio 04

---
+++ desafio01/04.js
@@ -0,0 +1,10 @@
+var distanciaPercorrida = 700
+var tempo = 10
+
+var velocidade = distanciaPercorrida / tempo
+
+var conversao = velocidade * 3.6
+
+var velocidade = conversao
+
+console.log (" A velocidade percorrida é: ", velocidade, "km")
