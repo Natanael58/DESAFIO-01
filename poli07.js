80a62831a866979b1cb38e7bb36a3072cac93898
Natanael Oliveira <natanolveira151@gmail.com>
2023-08-04 21:35:50 -0300

    poli

---
+++ poli07.js
@@ -0,0 +1,8 @@
+//Faça um programa que, a partir do número de lados de um polígono, imprima na tela a soma dos seus ângulos internos
+ let   numeroLadosPoligno = 6
+ 
+ let somarAngulo = (numeroLadosPoligno - 2) * 180
+
+ let angulo = somarAngulo / 6
+
+ console.log(" A soma dos angulos é ", somarAngulo, "É o vlor de cada angulo é", angulo)
