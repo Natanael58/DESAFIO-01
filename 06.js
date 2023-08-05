32a1a674614f479850f03f648512955c518030ea
Natanael Oliveira <natanolveira151@gmail.com>
2023-08-04 21:10:05 -0300

    06.js

---
+++ 06.js
@@ -0,0 +1,14 @@
+//Faça um programa que, a partir do raio de um círculo, calcula o comprimento (C) e a área (A) deste círculo.
+
+let raio = 3
+let pi = 3.14
+
+let comprimentoCirculo = 2 * pi * 1
+
+let areadoCirculo = pi * raio * raio
+
+console.log("O coprinto do  circulo é: ", comprimentoCirculo)
+
+console.log(" a area é: ", areadoCirculo)
+
+            
